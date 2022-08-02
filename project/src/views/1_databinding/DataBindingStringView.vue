00fb6fc477ec55b1ff62d2a6d1a4d02131f16348
Heechan Kang <weston0713@gmail.com>
2022-08-02 22:31:08 +0900

    databinding-string

---
+++ project/src/views/1_databinding/DataBindingStringView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1 class='text-red'>{{ userName }}</h1>
+    <h1 class='text-red'>{{ message }}</h1>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      userName: 'Jone Doe',
+      message: 'Hello~~',
+      arr: [],
+      obj: {}
+    }
+  }
+}
+</script>
+
+<style scoped>
+.text-red {
+  color: red;
+}
+</style>
