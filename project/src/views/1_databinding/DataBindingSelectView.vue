dd3e9fd43354f0666530a622a14cb52d0ca837de
Heechan Kang <weston0713@gmail.com>
2022-08-09 21:51:54 +0900

    selectbox

--- project/src/views/1_databinding/DataBindingSelectView.vue
+++ project/src/views/1_databinding/DataBindingSelectView.vue
@@ -1,12 +1,21 @@
 <template>
-  <div></div>
+  <div>
+    <select name="" id="" v-model="selectedCity">
+      <option value=""></option>
+      <option value="02">서울</option>
+      <option value="051">부산</option>
+      <option value="064">제주</option>
+    </select>
+    <span>{{ selectedCity }}</span>
+    <span>hello</span>
+  </div>
 </template>
 <script>
 export default {
   components: {},
   data () {
     return {
-      sampleData: ''
+      selectedCity: ''
     }
   },
   setup () {},
