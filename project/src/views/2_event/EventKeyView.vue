4986cd99a18aa842df61870643b5cccbe54a5d1f
Heechan Kang <weston0713@gmail.com>
2022-08-11 08:33:17 +0900

    vue done

---
+++ project/src/views/2_event/EventKeyView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <!-- <input type="search" name="" id="" @keyup="checkEnter($event)" v-model="searchText"> -->
+    <input type="search" name="" id="" @keyup.stop="doSearch" v-model="searchText">
+    <button @click="doSearch">조회</button>
+    <button type="submit" @click.prevent="doSearch">aa</button>
+  </div>
+</template>
+<script>
+export default {
+  components: {},
+  data () {
+    return {
+      searchText: ''
+    }
+  },
+  setup () {},
+  created () {},
+  mounted () {},
+  unmounted () {},
+  methods: {
+    doSearch() {
+      console.log(this.searchText)
+    },
+    checkEnter(event) {
+      if (event.keyCode === 13) {
+        this.doSearch()
+      }
+    }
+  }
+}
+</script>
